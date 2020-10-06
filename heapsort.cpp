8f336b46c4954fb53e90e6037cbc1f688f31233b
Joshua Williams <joshuaw1@usc.edu>
2020-10-06 12:18:44 -0700

    Group Work for Breakout Room 11 during class 10_06_20

--- heapsort.cpp
+++ heapsort.cpp
@@ -5,15 +5,21 @@ using namespace std;
 
 int main () 
 {
-  
+    // how to get a random integer between 0 and 999
+    int k = rand()%1000;
+    
+    cout<< "Sorting elements..." << endl;
+    // print integers in order from smallest to largest
+    priority_queue<int, vector<int>, greater<int>> elements;
+    
+    for (int i = 0; i < 10; ++i) {
+        elements.push(rand()%1000);
+    }
 
-  
-  // how to get a random integer between 0 and 999
-      int k = rand()%1000;
-    
-  
-  cout<< "Sorting elements..." << endl;
- // print integers in order from smallest to largest
-    
-    
+    while (!elements.empty()) {
+        cout << elements.top() << endl;
+        elements.pop();
+    }
+
+
 }
